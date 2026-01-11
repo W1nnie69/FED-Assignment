d9092b37aa9a4d2093e732153473de0312059ec9
W1nnie69 <danizye@gmail.com>
2026-01-12 03:06:53 +0800

    testing a very basic access control function in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // code goes here
+
+// very basic access controls
+// if user is a "patron", they will not be able to access the vendor dashboard.
+// Sending them back to index.html
+const user = { role: "patron" };
+
+function checkAccess(requiredRole) {
+  if (user.role !== requiredRole) {
+    alert("Access Denied! Redirecting to home page.");
+    window.location.href = "index.html"; // redirect
+  }
+}
+
+checkAccess("vendor");
