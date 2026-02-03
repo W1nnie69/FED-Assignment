a99468f11c1d4118d6fc4d803d623660cfaeaaef
jjustin282 <justinang282@gmail.com>
2026-02-03 09:22:59 +0800

    first version of nea login page, add nea officer for login.html

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("form");
+const submitbtn = form.document.querySelector(".submitbtn");
+const inputname = form.document.querySelector(".input-name").value;
+const inputid = form.document.querySelector(".input-id").value;
