5c4e0a4acd89cad67fc12ce4003a20878104c451
W1nnie69 <danizye@gmail.com>
2026-02-07 22:47:48 +0800

    Moved everything to the same format as main.

--- js/script.js
+++
@@ -1,4 +0,0 @@
-const form = document.querySelector("form");
-const submitbtn = form.document.querySelector(".submitbtn");
-const inputname = form.document.querySelector(".input-name").value;
-const inputid = form.document.querySelector(".input-id").value;
