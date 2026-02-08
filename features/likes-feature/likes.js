b6946c725028d767566a1d2e0bda4dfccc086330
Jeff-sato <leej13463@gmail.com>
2026-02-08 11:15:55 +0800

    did the profile menu to link the features

--- features/likes-feature/likes.js
+++ features/likes-feature/likes.js
@@ -1,6 +1,34 @@
 const likeButtons = document.querySelectorAll(".like-btn");
+const profileIcon = document.getElementById("profileIcon");
+const profileMenu = document.getElementById("profileMenu");
+const profileLinks = document.querySelectorAll(".profile-link");
 
 
+profileIcon.addEventListener("click", (e) => {
+    e.stopPropagation(); // prevent click from closing immediately
+    profileMenu.classList.toggle("hidden");
+});
+
+// Close the menu if clicking outside
+document.addEventListener("click", () => {
+    if (!profileMenu.classList.contains("hidden")) {
+        profileMenu.classList.add("hidden");
+    }
+});
+
+profileLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        const text = link.textContent.trim();
+
+        if (text === "Promotion") {
+            window.location.href = "../promotion-page/promotion.html";
+        }
+
+        if (text === "Review") {
+            window.location.href = "../review-page-feature/review_page.html";
+        }
+    });
+});
 
 // Loop through each like button
 likeButtons.forEach(button => 
